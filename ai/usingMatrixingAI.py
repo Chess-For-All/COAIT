6dd6a465a152adbba9648624ea4f1e4117f1aaf5
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2025-04-11 18:11:31 +0400

    Create usingMatrixingAI.py

---
+++ ai/usingMatrixingAI.py
@@ -0,0 +1,12 @@
+async def pp(file, n):
+    pFile = file
+    X = pFile.url
+    Y = pFile.data
+    if n != None:
+        return pFile
+        return X
+        return Y
+
+for i in range(infinity):
+    pp(COAIT/ai/matrix_ai.py, i)
+    print(pFile, X, Y)
