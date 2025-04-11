e5fbe3f2f64b0864b8f0760040269baff42c60c5
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2025-04-11 15:42:31 +0400

    Create SF.py

---
+++ analysis/SF.py
@@ -0,0 +1,11 @@
+def getCode(path, url):
+    path.url = url
+    C = path.url
+    code = C.data 
+    return code
+
+for i in range(infinity):
+    if __name__ == "__main__":
+        getCode(official-stockfish/Stockfish, https://github.com/official-stockfish/Stockfish)
+        print(code)
+      
