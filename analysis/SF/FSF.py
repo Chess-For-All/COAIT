ecd8cb13e0dba2ed789a707258ad973fca721a9d
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2025-04-15 19:47:36 +0400

    Create FSF.py

---
+++ analysis/SF/FSF.py
@@ -0,0 +1,11 @@
+def getCode(path, url):
+    path.url = url
+    C = path.url
+    code = C.data 
+    return code
+
+for i in range(infinity):
+    if __name__ == "__main__":
+        getCode(fairy-stockfish/Fairy-Stockfish, https://github.com/fairy-stockfish/Fairy-Stockfish)
+        print(code)
+      
